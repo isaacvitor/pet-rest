eaa3bcf586787fefd48b6621264db6f801440276
Isaac Silva <isaac.silva@kuehne-nagel.com>
2024-12-20 11:45:43 +0000

    Add spring project

---
+++ src/main/java/com/pet/rest/PetRestApplication.java
@@ -0,0 +1,13 @@
+package com.pet.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetRestApplication.class, args);
+	}
+
+}
